af303c108342b5061d943a31127b7ea2b06b7f21
Taylor McCoy <mccoyt57@gmail.com>
2024-12-27 11:07:12 -0600

    testing remote

--- sample_debug.py
+++ sample_debug.py
@@ -4,33 +4,3 @@ y_trying = 15
 z = x_trying + y_trying
 
 print(z)
-
-print("""
-dfkjxlknmcvlisd
-fdkjfsoieudjfeskfnmsl
-kcvlksdfjsdklj
-dfkjxlknmcvlisd
-dfkjxlknmcvlisd
-dfkjxlknmcvlisd
-dfkjxlknmcvlisd
-dfkjxlknmcvlisd
-dfkjxlknmcvlisd
-dfkjxlknmcvlisd
-dfkjxlknmcvlisd
-fdkjfsoieudjfeskfnmsl
-kcvlksdfjsdklj
-fdkjfsoieudjfeskfnmsl
-kcvlksdfjsdklj
-fdkjfsoieudjfeskfnmsl
-kcvlksdfjsdklj
-fdkjfsoieudjfeskfnmsl
-kcvlksdfjsdklj
-fdkjfsoieudjfeskfnmsl
-kcvlksdfjsdklj
-fdkjfsoieudjfeskfnmsl
-kcvlksdfjsdklj
-fdkjfsoieudjfeskfnmsl
-kcvlksdfjsdklj
-fdkjfsoieudjfeskfnmsl
-kcvlksdfjsdklj
-""")
