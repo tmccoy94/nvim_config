6cf7c374047f554dee6c9d7187caf026602934fb
Taylor McCoy <mccoyt57@gmail.com>
2024-12-27 10:18:00 -0600

    Config terminal setup
    Deleted out unecessary tmux, deleted unused plugins file, set esc
    terminal bind as with all other nvim windows.

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,4 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = "Esc key to exit :term"})
